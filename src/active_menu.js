d666d7944db6d0093a8573adfff6d1d81c8f53e9
felixhsm <felixhsm9410@gmail.com>
2024-03-21 17:14:09 +0900

    Observer-part1

--- src/active_menu.js
+++ src/active_menu.js
@@ -5,17 +5,42 @@
 // 보여지는 섹션: 
 // - 다수의 섹션이 동시에 보여진다면, 가장 첫번째 섹션을 선택
 // - 마지막 contact 섹션이 보여진다면, 그러면 가장 마지막 섹션을 선택
-const sectionIds = ['#home', '#about', '#skills', '#work', '#testimonial', '#contact'];
+const sectionIds = [
+'#home',
+'#about',
+'#skills',
+'#work',
+'#testimonial',
+'#contact',
+];
 const sections = sectionIds.map((id) => document.querySelector(id));
-const navItems = sectionIds.map(id => document.querySelector(`[href = "${id}"]`))
+const navItems = sectionIds.map((id) => document.querySelector(`[href = "${id}"]`));
+const visibleSections = sectionIds.map(() => false);
+
 const options = {};
 const observer = new IntersectionObserver(observerCallback , options);
-sections.forEach(section => observer.observe(section));
+sections.forEach((section) => observer.observe(section));
 
 function observerCallback(entries) {
-    entries.forEach(entry => {
-        console.log(entry.target);
-        console.log(entry.isIntersecting);
-        console.log(entry.intersectionRatio);
-    })
+    let selectLastOne; // flag 변수
+    entries.forEach((entry) => {
+        const index = sectionIds.indexOf(`#${entry.target.id}`);
+        visibleSections[index] = entry.isIntersecting;
+        selectLastOne = 
+        index === sectionIds.length - 1 && 
+        entry.isIntersecting && 
+        entry.intersectionRatio >= 0.99;
+    });
+    console.log(visibleSections);
+    console.log('무조건 라스트 섹션!', selectLastOne);
+
+    const navIndex = selectLastOne ? 
+    sectionIds.length - 1 : 
+    findFirstIntersecting(visibleSections);
+    console.log(sectionIds[navIndex]);
+}
+
+function findFirstIntersecting(intersections) {
+    const index = intersections.indexOf(true);
+    return index >= 0 ? index : 0
 }
